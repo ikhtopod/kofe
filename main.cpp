4047634f811ba36f227f9cd1d1d41b393c1e473a
reclue <vitaly@lifanoff.ru>
2020-12-29 20:43:19 +0300

    added the GLM library

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include "glm/gtx/normal.hpp"
+#include "glm/gtx/transform.hpp"
+#include "glm/gtx/quaternion.hpp"
+#include "glm/gtx/euler_angles.hpp"
+#include "glm/gtx/matrix_decompose.hpp"
+
 
 int main() {
     std::cout << "Hello, The Seductive Dream engine!" << std::endl;
