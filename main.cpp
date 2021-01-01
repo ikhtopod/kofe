605a00e160f9a4e549f60b560db5a3b0fd42d644
reclue <vitaly@lifanoff.ru>
2021-01-01 20:28:30 +0300

    some changes

--- main.cpp
+++ main.cpp
@@ -18,9 +18,6 @@
 #include "glm/gtx/matrix_decompose.hpp"
 
 
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_ONLY_PNG
-
 #include <stb_image.h>
 
 
---
+++ thirdparty/stb/sources/stb_image.c
@@ -0,0 +1,4 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_ONLY_PNG
+
+#include "../headers/stb_image.h"
