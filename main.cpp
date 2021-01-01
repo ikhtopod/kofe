e8b47f3eef2d8c923ebd51f29e19fb4aa78230eb
reclue <vitaly@lifanoff.ru>
2021-01-01 22:48:29 +0300

    added pch

--- main.cpp
+++ main.cpp
@@ -1,27 +1,5 @@
-#include <cstdlib>
-#include <iostream>
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-
-#define GLM_ENABLE_EXPERIMENTAL
-
-#include "glm/gtx/normal.hpp"
-#include "glm/gtx/transform.hpp"
-#include "glm/gtx/quaternion.hpp"
-#include "glm/gtx/euler_angles.hpp"
-#include "glm/gtx/matrix_decompose.hpp"
-
-
-#include <stb_image.h>
-
-
 int main() {
-    std::cout << "Hello, The Seductive Dream engine!" << std::endl;
+    std::string message { "Hello, The Seductive Dream engine!" };
+    std::cout << message << std::endl;
     return EXIT_SUCCESS;
 }
