bb43c1c88b08e1573451bc058583efeff4318f1d
reclue <vitaly@lifanoff.ru>
2021-01-15 19:11:09 +0300

    added the DeltaTime class

---
+++ lib/core/deltatime.h
@@ -0,0 +1,43 @@
+#ifndef DELTATIME_H
+#define DELTATIME_H
+
+#include "icanbeeverywhere.h"
+
+#include <ratio>
+#include <chrono>
+#include <string>
+
+
+class DeltaTime final : public ICanBeEverywhere {
+    using Type = float;
+    using Ratio = std::ratio<1>;
+    using Duration = std::chrono::duration<Type, Ratio>;
+    using Clock = std::chrono::steady_clock;
+    using TimePoint = std::chrono::time_point<Clock, Duration>;
+
+public:
+    friend void swap(DeltaTime&, DeltaTime&);
+
+private:
+    TimePoint m_prevTime;
+    Type m_delta;
+
+public:
+    DeltaTime();
+    DeltaTime(const DeltaTime& other);
+    DeltaTime(DeltaTime&& other) noexcept;
+    DeltaTime& operator=(const DeltaTime& other);
+    DeltaTime& operator=(DeltaTime&& other) noexcept;
+    virtual ~DeltaTime() = default;
+
+public:
+    void Update();
+    Type GetDelta() const;
+
+public:
+    static std::string ToString();
+};
+
+void swap(DeltaTime& lhs, DeltaTime& rhs);
+
+#endif // DELTATIME_H
