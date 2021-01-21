ee5653d267fd2cb8315a1def3123003c924f949d
reclue <vitaly@lifanoff.ru>
2021-01-21 16:56:50 +0300

    added the Transform class

---
+++ lib/core/transform.cpp
@@ -0,0 +1,122 @@
+#include "transform.h"
+
+#include "axis.h"
+
+#include <glm/matrix.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+
+#include <utility>
+
+
+void swap(Transform& lhs, Transform& rhs) {
+    if (&lhs == &rhs) return;
+
+    using std::swap;
+
+    swap(lhs.m_position, rhs.m_position);
+    swap(lhs.m_rotation, rhs.m_rotation);
+    swap(lhs.m_scale, rhs.m_scale);
+}
+
+
+const glm::vec3 Transform::DEFAULT_POSITION { 0.0f };
+const glm::vec3 Transform::DEFAULT_ROTATION { 0.0f };
+const glm::vec3 Transform::DEFAULT_SCALE { 1.0f };
+
+Transform::Transform() :
+    Transform { DEFAULT_POSITION,
+                DEFAULT_ROTATION,
+                DEFAULT_SCALE } {}
+
+Transform::Transform(const Transform& other) :
+    Transform { other.m_position,
+                other.m_rotation,
+                other.m_scale } {}
+
+Transform::Transform(Transform&& other) noexcept :
+    Transform { std::move(other.m_position),
+                std::move(other.m_rotation),
+                std::move(other.m_scale) } {}
+
+Transform& Transform::operator=(const Transform& other) {
+    if (this != &other) {
+        m_position = other.m_position;
+        m_rotation = other.m_rotation;
+        m_scale = other.m_scale;
+    }
+
+    return *this;
+}
+
+Transform& Transform::operator=(Transform&& other) noexcept {
+    if (this != &other) {
+        m_position = std::move(other.m_position);
+        m_rotation = std::move(other.m_rotation);
+        m_scale = std::move(other.m_scale);
+    }
+
+    return *this;
+}
+
+Transform::Transform(const glm::vec3& position,
+                     const glm::vec3& rotation,
+                     const glm::vec3& scale) :
+    m_position { position },
+    m_rotation { rotation },
+    m_scale { scale } {}
+
+Transform::Transform(glm::vec3&& position,
+                     glm::vec3&& rotation,
+                     glm::vec3&& scale) noexcept :
+    m_position { std::move(position) },
+    m_rotation { std::move(rotation) },
+    m_scale { std::move(scale) } {}
+
+void Transform::Reset() {
+    *this = Transform {};
+}
+
+glm::vec3 Transform::GetPosition() const {
+    return m_position;
+}
+
+void Transform::SetPosition(const glm::vec3& position) {
+    m_position = position;
+}
+
+glm::vec3 Transform::GetRotation() const {
+    return m_rotation;
+}
+
+void Transform::SetRotation(const glm::vec3& rotation) {
+    m_rotation = rotation;
+}
+
+glm::vec3 Transform::GetScale() const {
+    return m_scale;
+}
+
+void Transform::SetScale(const glm::vec3& scale) {
+    m_scale = scale;
+}
+
+glm::mat4 Transform::GetPositionMatrix() const {
+    return glm::translate(m_position);
+}
+
+glm::mat4 Transform::GetRotationMatrix() const {
+    glm::mat4 rotX = glm::rotate(glm::radians(m_rotation.x), Axis::RIGHT);
+    glm::mat4 rotY = glm::rotate(glm::radians(m_rotation.y), Axis::UP);
+    glm::mat4 rotZ = glm::rotate(glm::radians(m_rotation.z), Axis::FRONT);
+
+    return rotX * rotY * rotZ;
+}
+
+glm::mat4 Transform::GetScaleMatrix() const {
+    return glm::scale(glm::mat4 {}, m_scale);
+}
+
+glm::mat4 Transform::ToMatrix() const {
+    return GetPositionMatrix() * GetRotationMatrix() * GetScaleMatrix();
+}
---
+++ lib/core/transform.h
@@ -0,0 +1,59 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+#include <glm/glm.hpp>
+
+
+class Transform final {
+    static const glm::vec3 DEFAULT_POSITION;
+    static const glm::vec3 DEFAULT_ROTATION;
+    static const glm::vec3 DEFAULT_SCALE;
+
+private:
+    glm::vec3 m_position;
+    glm::vec3 m_rotation;
+    glm::vec3 m_scale;
+
+public:
+    friend void swap(Transform& lhs, Transform& rhs);
+
+public:
+    Transform();
+    Transform(const Transform& other);
+    Transform(Transform&& other) noexcept;
+    Transform& operator=(const Transform& other);
+    Transform& operator=(Transform&& other) noexcept;
+    ~Transform() = default;
+
+    Transform(const glm::vec3& position,
+              const glm::vec3& rotation,
+              const glm::vec3& scale);
+
+    Transform(glm::vec3&& position,
+              glm::vec3&& rotation,
+              glm::vec3&& scale) noexcept;
+
+public:
+    void Reset();
+
+    glm::vec3 GetPosition() const;
+    void SetPosition(const glm::vec3& position);
+
+    glm::vec3 GetRotation() const;
+    void SetRotation(const glm::vec3& rotation);
+
+    glm::vec3 GetScale() const;
+    void SetScale(const glm::vec3& scale);
+
+public:
+    glm::mat4 GetPositionMatrix() const;
+    glm::mat4 GetRotationMatrix() const;
+    glm::mat4 GetScaleMatrix() const;
+
+    glm::mat4 ToMatrix() const;
+};
+
+
+void swap(Transform& lhs, Transform& rhs);
+
+#endif // TRANSFORM_H
