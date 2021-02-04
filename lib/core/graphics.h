7d4d797cf19c8637e038dd35efd098530afcbf90
reclue <vitaly@lifanoff.ru>
2021-02-04 22:55:50 +0300

    added the Graphics class base for the OpenGL class

---
+++ lib/core/graphics.h
@@ -0,0 +1,30 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include "iprocess.h"
+#include "icanbeeverywhere.h"
+
+
+class Graphics :
+        public IProcess,
+        public ICanBeEverywhere
+{
+public:
+    Graphics(const Graphics&) = delete;
+    Graphics(Graphics&&) noexcept = delete;
+    Graphics& operator=(const Graphics&) = delete;
+    Graphics& operator=(Graphics&&) noexcept = delete;
+
+public:
+    Graphics() = default;
+    virtual ~Graphics() = default;
+
+protected:
+    virtual void Init() = 0;
+
+public:
+    virtual void UpdateViewportSize() const = 0;
+    virtual void Flush() const = 0;
+};
+
+#endif // GRAPHICS_H
