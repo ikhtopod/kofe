8581935c8731ca363d16c77f285592b34510bf49
reclue <vitaly@lifanoff.ru>
2021-02-18 00:47:07 +0300

    updated Transformable

--- lib/headers/core/transform/transformable.h
+++ lib/headers/core/transform/transformable.h
@@ -6,15 +6,29 @@
 
 
 class Transformable :
-    public LocalTransformation,
-    public ParentTransformation {
+    public ParentTransformation,
+    public LocalTransformation {
 public:
     Transformable();
+    Transformable(const Transformable& other);
+    Transformable(Transformable&& other) noexcept;
+    Transformable& operator=(const Transformable& other);
+    Transformable& operator=(Transformable&& other) noexcept;
     virtual ~Transformable() = default;
+
+public:
+    explicit Transformable(const Transform& parent, const Transform& local);
+    explicit Transformable(const ParentTransformation& parent, const LocalTransformation& local);
 
 public:
     virtual Transform GetGlobalTransform();
     virtual const Transform GetGlobalTransform() const;
+
+public:
+    friend void swap(Transformable&, Transformable&);
 };
+
+
+void swap(Transformable& lhs, Transformable& rhs);
 
 #endif // TRANSFORMABLE_H
--- lib/sources/core/transform/transformable.cpp
+++ lib/sources/core/transform/transformable.cpp
@@ -1,9 +1,55 @@
 #include "transform/transformable.h"
 
+#include <utility>
+
+
+void swap(Transformable& lhs, Transformable& rhs) {
+    if (&lhs == &rhs) return;
+
+    using std::swap;
+
+    swap(lhs.m_parentTransform, rhs.m_parentTransform);
+    swap(lhs.m_localTransform, rhs.m_localTransform);
+}
 
 Transformable::Transformable() :
-    LocalTransformation {},
-    ParentTransformation {} {}
+    ParentTransformation {},
+    LocalTransformation {} {}
+
+
+Transformable::Transformable(const Transformable& other) :
+    ParentTransformation { other.m_parentTransform },
+    LocalTransformation { other.m_localTransform } {}
+
+Transformable::Transformable(Transformable&& other) noexcept :
+    ParentTransformation { std::move(other.m_parentTransform) },
+    LocalTransformation { std::move(other.m_localTransform) } {}
+
+Transformable& Transformable::operator=(const Transformable& other) {
+    if (this != &other) {
+        m_parentTransform = std::move(other.m_parentTransform);
+        m_localTransform = std::move(other.m_localTransform);
+    }
+
+    return *this;
+}
+
+Transformable& Transformable::operator=(Transformable&& other) noexcept {
+    if (this != &other) {
+        m_parentTransform = other.m_parentTransform;
+        m_localTransform = other.m_localTransform;
+    }
+
+    return *this;
+}
+
+Transformable::Transformable(const Transform& parent, const Transform& local) :
+    ParentTransformation { parent },
+    LocalTransformation { local } {}
+
+Transformable::Transformable(const ParentTransformation& parent, const LocalTransformation& local) :
+    ParentTransformation { parent },
+    LocalTransformation { local } {}
 
 Transform Transformable::GetGlobalTransform() {
     return GetParentTransform() + GetTransform();
